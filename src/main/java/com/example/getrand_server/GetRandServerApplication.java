952f19758f38c4692edd7c49f08f7b960cff0321
CoderJDan <coder.jangdaniel@gmail.com>
2024-12-22 17:29:36 +0900

    first commit

---
+++ src/main/java/com/example/getrand_server/GetRandServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.getrand_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class GetRandServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetRandServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/getrand_server/GetRandServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getrand_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetRandServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
